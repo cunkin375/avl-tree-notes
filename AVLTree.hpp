e5c22dc059acdb925b71a2a49405cc696510cd2b
ali <159713281+cunkin375@users.noreply.github.com>
2025-11-02 22:19:07 -0500

    replaced .cpp with .tpp template file

--- AVLTree.hpp
+++ AVLTree.hpp
@@ -12,7 +12,7 @@ private:
         Node_* left_;
         Node_* right_;
 
-        Node_(T incoming_data) : 
+        Node_(T& incoming_data) : 
             data_(incoming_data), 
             height_(0),
             left_(nullptr), right_(nullptr)
@@ -22,9 +22,7 @@ private:
 
 public:
     // TODO: Fix constructor
-    AVLTree(T value) : root_(insert()) 
-    {
-    }
+    AVLTree(T value);
     ~AVLTree();
 
     T remove_value(std::unique_ptr<T> value);
@@ -37,14 +35,14 @@ private:
     // source: Herb Sutter "Back to the Basics! Essentials of Modern C++ Style"
     std::unique_ptr<Node_> rebalance(std::unique_ptr<Node_>& root);
 
-    Node_* find_delete_ptr(Node_& root);
-    Node_* max_left(Node_& root);
+    Node_* find_delete_ptr(Node_* root);
+    Node_* max_left(Node_* root);
 
     void delete_node();
     void update_height();
 
-    bool is_leaf_node(Node_& root) const;
-    bool has_two_children(Node_& root) const;
+    bool is_leaf_node(Node_* root) const;
+    bool has_two_children(Node_* root) const;
 
 private:
     std::unique_ptr<Node_> root_;
